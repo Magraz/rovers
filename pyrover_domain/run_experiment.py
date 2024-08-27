ff4a70005be887cd616722ac52a46c822390472c
Magraz <agrazvallejo@live.com>
2024-08-26 18:10:26 -0700

    added hpc argument

--- pyrover_domain/run_experiment.py
+++ pyrover_domain/run_experiment.py
@@ -2,6 +2,9 @@ from pyrover_domain.ccea.ccea_lib import runCCEA
 import torch.multiprocessing as mp
 from torch import set_grad_enabled
 
+import argparse
+
+
 if __name__ == "__main__":
     # Set for CUDA to allow multiprocessing
     mp.set_start_method("forkserver")
@@ -9,8 +12,18 @@ if __name__ == "__main__":
     # Disable gradient calculations
     set_grad_enabled(False)
 
+    # Arg parser variables
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hpc", help="use hpc config files")
+    args = parser.parse_args()
+
+    # Set base_config path
+    config_dir = "/home/magraz/rovers/pyrover_domain/config/experiments"
+    if args.hpc:
+        config_dir = "/nfs/stak/users/agrazvam/rovers/pyrover_domain/config/experiments/hpc"
+
     # Set configuration file
-    config_dir = "/home/magraz/rovers/pyrover_domain/config/multi_agent_regular_mlp.yaml"
+    config_dir = f"{config_dir}/multi_agent_regular_mlp.yaml"
     # config_dir = "/home/magraz/rovers/pyrover_domain/config/single_agent_decay.yaml"
     # config_dir = "/home/magraz/rovers/pyrover_domain/config/multi_agent_decay_mlp.yaml"
     # config_dir = "/home/magraz/rovers/pyrover_domain/config/multi_agent_decay_cnn.yaml"
