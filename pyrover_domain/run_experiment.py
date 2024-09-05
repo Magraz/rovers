cc0627beb9f314374da253e630267f392d01917a
Magraz <agrazvallejo@live.com>
2024-09-04 22:59:33 -0700

    added arguments

--- pyrover_domain/run_experiment.py
+++ pyrover_domain/run_experiment.py
@@ -1,4 +1,4 @@
-from pyrover_domain.ccea.ccea_lib import runCCEA
+from pyrover_domain.algorithms.ccea import runCCEA
 import torch.multiprocessing as mp
 from torch import set_grad_enabled
 
@@ -14,21 +14,25 @@ if __name__ == "__main__":
 
     # Arg parser variables
     parser = argparse.ArgumentParser()
-    parser.add_argument("--hpc", help="use hpc config files", action="store_true")
-    args = parser.parse_args()
+    parser.add_argument("--hpc", default=False, help="use hpc config files", action="store_true")
+    parser.add_argument("--teaming", default=True, help="use teaming", action="store_true")
+    parser.add_argument("--modality", default="multi", help="multi/single", type=str)
+    parser.add_argument("--poi_type", default="static", help="static/decay", type=str)
+    parser.add_argument("--model", default="mlp", help="mlp/gru/cnn", type=str)
+
+    args = vars(parser.parse_args())
 
     # Set base_config path
-    config_dir = "/home/magraz/rovers/pyrover_domain/experiments/yamls"
-    if args.hpc:
-        config_dir = "/nfs/hpc/share/agrazvam/experiments/yamls"
+    config_dir = (
+        "/nfs/hpc/share/agrazvam/experiments/yamls"
+        if args["hpc"]
+        else "/home/magraz/rovers/pyrover_domain/experiments/yamls"
+    )
 
-    model = "mlp"  # mlp/cnn/gru
-    modality = "multi"  # single/multi
-    poi_type = "regular"  # regular/decay
-    formations = "teaming"  # teaming/''
+    teaming = "_teaming" if args["teaming"] else ""
 
     # Set configuration file
-    config_dir = f"{config_dir}/{modality}_agent_{poi_type}_{model}_{formations}.yaml"
+    config_dir = f"{config_dir}/{args['modality']}_{args['poi_type']}_{args['model']}{teaming}.yaml"
 
     # Run learning algorithm
     runCCEA(config_dir=config_dir)
